3697850de4208549725d72ea7551ab11b7c59dc6
A.Leon <A.Leon@AC-ALEON>
2022-06-13 23:28:19 +0200

    Add project files.

---
+++ Models/Contacts.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    public class Contacts
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ Rubrica/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Rubrica.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
